c0b54d5941bc29d9a56a9969cf269b9f7d588b5a
root <root@ce-front0.dev.kaltura.com>
2015-03-20 00:23:38 -0400

    New clientlib ver

--- cli/kalcli.php
+++ cli/kalcli.php
@@ -137,7 +137,7 @@ if (count($arguments) < 2)
 $service = trim($arguments[0]);
 $action = trim($arguments[1]);
 
-$params = array('clientTag' => 'kalcli:15-03-19');
+$params = array('clientTag' => 'kalcli:15-03-20');
 $extraArgCount = count($arguments);
 for ($curIndex = 2; $curIndex < $extraArgCount; $curIndex++)
 {
