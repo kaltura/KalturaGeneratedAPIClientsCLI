e0d302286c079b6b7954fa8ac6f4a4f8a63a6686
Kaltura CSI <community@kaltura.com>
2015-04-25 12:05:43 -0400

    New clientlib ver

--- setup.sh
+++ setup.sh
@@ -22,4 +22,4 @@ if [ `id -u` = 0 ] ;then
     ln -sf $PREFIX/kalcliAutoComplete /etc/bash_completion.d/
     ln -sf $PREFIX/kalcliAliases.sh /etc/profile.d/
 fi
-sed  -e "s#54.91.3.112#$API_HOST#g" -e "s#~/kaltura/log#$PREFIX/log#g" -e "s#101#$PARTNER_ID#g" -e "s#066439f83615896a16efce529b84fcfb#$ADMIN_SECRET#g" $PREFIX/config/config.template.ini > $PREFIX/config/config.ini
+sed  -e "s#54.160.105.103#$API_HOST#g" -e "s#~/kaltura/log#$PREFIX/log#g" -e "s#101#$PARTNER_ID#g" -e "s#47a7f46ec0b389dba597fca30a78014d#$ADMIN_SECRET#g" $PREFIX/config/config.template.ini > $PREFIX/config/config.ini
