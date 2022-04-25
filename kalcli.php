003fade6e64a7215b4b3e75b752697d21a635936
Kaltura CSI <community@kaltura.com>
2022-04-25 19:29:24 -0400

    New clientlib ver

--- kalcli.php
+++ kalcli.php
@@ -187,7 +187,7 @@ if (count($arguments) < 2)
 $service = trim($arguments[0]);
 $action = trim($arguments[1]);
 
-$params = array('clientTag' => 'kalcli:22-04-24');
+$params = array('clientTag' => 'kalcli:22-04-25');
 $extraArgCount = count($arguments);
 for ($curIndex = 2; $curIndex < $extraArgCount; $curIndex++)
 {
